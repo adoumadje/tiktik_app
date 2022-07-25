3f9877bbf41087dab15c3f889efde3f115ad99ec
adoumadje <adoumadjecaleb@gmail.com>
2022-07-25 03:44:25 +0300

    Main Video View 1h16mn

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -1,6 +1,7 @@
+import React from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import React from 'react'
+
 import { topics } from '../utils/constants'
 
 function Discover() {
